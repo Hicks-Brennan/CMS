2f469026b13209920c2af55c7f8824c3fc521d5c
Brennan <hic11001@byui.edu>
2020-05-09 17:08:04 -0700

    message edit added

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../messages.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  @ViewChild('subject', {static: false}) subjectRef: ElementRef;
+  @ViewChild('message', {static:false}) messageRef: ElementRef;
+  @Output() messageAdded = new EventEmitter<Message>();
+  currentSender: string = 'Brennan';
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSendMessage() {
+    const msgSubject = this.subjectRef.nativeElement.value;
+    const msgMessage = this.messageRef.nativeElement.value;
+    const newMessage = new Message(1, msgSubject, msgMessage, this.currentSender);
+    this.messageAdded.emit(newMessage)
+  }
+
+  onClear() {
+    this.subjectRef.nativeElement.value = '';
+    this.messageRef.nativeElement.value = '';
+  }
+
 
 }
--- src/app/messages/messages.model.ts
+++ src/app/messages/messages.model.ts
@@ -1,4 +1,4 @@
-export class Messages {
+export class Message {
     constructor( public messageId: number,
         public subject: string,
         public msgText: string,
